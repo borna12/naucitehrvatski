a8f32b1565d23bd500e6f55a8e0d06c671e8741e
Josip <jomihx@gmail.com>
2019-12-12 23:57:30 +0100

    gurni

---
+++ wp-login.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="hr">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="hr">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Prijava &lsaquo; Naučite hrvatski &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://borna12.github.io/naucitehrvatski/wp-includes/css/dashicons.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://borna12.github.io/naucitehrvatski/wp-includes/css/buttons.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://borna12.github.io/naucitehrvatski/wp-admin/css/forms.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://borna12.github.io/naucitehrvatski/wp-admin/css/l10n.min.css?ver=5.3' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://borna12.github.io/naucitehrvatski/wp-admin/css/login.min.css?ver=5.3' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-hr">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Pokreće WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://borna12.github.io/naucitehrvatski/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Korisničko ime ili adresa e-pošte</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Lozinka</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Prikaži lozinku">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Zapamti me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Prijava" />
+									<input type="hidden" name="redirect_to" value="https://borna12.github.io/naucitehrvatski/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+									<a href="https://borna12.github.io/naucitehrvatski/wp-login.php?action=lostpassword">Izgubili ste lozinku?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="https://borna12.github.io/naucitehrvatski/">
+		&larr; Povratak na Naučite hrvatski		</a></p>
+			</div>
+	<script src='https://borna12.github.io/naucitehrvatski/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script src='https://borna12.github.io/naucitehrvatski/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script>
+var _zxcvbnSettings = {"src":"https:\/\/borna12.github.io\/naucitehrvatski\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src='https://borna12.github.io/naucitehrvatski/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script>
+var pwsL10n = {"unknown":"Nepoznata ja\u010dina lozinke","short":"Vrlo slabo","bad":"Slabo","good":"Srednja","strong":"Jako","mismatch":"Neslaganje"};
+</script>
+<script src='https://borna12.github.io/naucitehrvatski/wp-admin/js/password-strength-meter.min.js?ver=5.3'></script>
+<script src='https://borna12.github.io/naucitehrvatski/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script>
+var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src='https://borna12.github.io/naucitehrvatski/wp-includes/js/wp-util.min.js?ver=5.3'></script>
+<script>
+var userProfileL10n = {"warn":"Va\u0161a nova lozinka nije spremljena.","warnWeak":"Potvrdite upotrebu slabe lozinke","show":"Prika\u017ei","hide":"Sakrij","cancel":"Odustani","ariaShow":"Prika\u017ei lozinku","ariaHide":"Sakrij lozinku"};
+</script>
+<script src='https://borna12.github.io/naucitehrvatski/wp-admin/js/user-profile.min.js?ver=5.3'></script>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		<div class="clear"></div>
+	</body>
+	</html>
+	
