2f9d62e4d8f5e3190d8aaf28d7be42d8260331b4
Josip <jomihx@gmail.com>
2020-02-27 19:03:11 +0100

    ajd da vidimo

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Prijava &lsaquo; Naučite hrvatski &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://borna12.github.io/naucitehrvatski/wp-includes/css/dashicons.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://borna12.github.io/naucitehrvatski/wp-includes/css/buttons.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://borna12.github.io/naucitehrvatski/wp-admin/css/forms.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://borna12.github.io/naucitehrvatski/wp-admin/css/l10n.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://borna12.github.io/naucitehrvatski/wp-admin/css/login.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='./wp-includes/css/dashicons.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='./wp-includes/css/buttons.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='./wp-admin/css/forms.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='./wp-admin/css/l10n.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='./wp-admin/css/login.min.css?ver=5.3.2' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -25,7 +25,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Pokreće WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://borna12.github.io/naucitehrvatski/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="./wp-login.php" method="post">
 			<p>
 				<label for="user_login">Korisničko ime ili adresa e-pošte</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -43,48 +43,48 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Zapamti me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Prijava" />
-									<input type="hidden" name="redirect_to" value="https://borna12.github.io/naucitehrvatski/wp-admin/" />
+									<input type="hidden" name="redirect_to" value="./wp-admin/" />
 									<input type="hidden" name="testcookie" value="1" />
 			</p>
 		</form>
 
 					<p id="nav">
-									<a href="https://borna12.github.io/naucitehrvatski/wp-login.php?action=lostpassword">Izgubili ste lozinku?</a>
+									<a href="./wp-login.php?action=lostpassword">Izgubili ste lozinku?</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="https://borna12.github.io/naucitehrvatski/">
+				<p id="backtoblog"><a href="./index.html">
 		&larr; Povratak na Naučite hrvatski		</a></p>
 			</div>
-	<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+	<script type='text/javascript' src='./wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='./wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"https:\/\/borna12.github.io\/naucitehrvatski\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":".\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script type='text/javascript' src='./wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"Nepoznata ja\u010dina lozinke","short":"Vrlo slabo","bad":"Slabo","good":"Srednja","strong":"Jako","mismatch":"Neslaganje"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
-<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script type='text/javascript' src='./wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
+<script type='text/javascript' src='./wp-includes/js/underscore.min.js?ver=1.8.3'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
+<script type='text/javascript' src='./wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var userProfileL10n = {"warn":"Va\u0161a nova lozinka nije spremljena.","warnWeak":"Potvrdite upotrebu slabe lozinke","show":"Prika\u017ei","hide":"Sakrij","cancel":"Odustani","ariaShow":"Prika\u017ei lozinku","ariaHide":"Sakrij lozinku"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='https://borna12.github.io/naucitehrvatski/wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
+<script type='text/javascript' src='./wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
 	<div class="clear"></div>
 	</body>
 	</html>
